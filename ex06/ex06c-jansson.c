c4c9212a30623e0e62b49cc0ddee4da89482613d
Basile Starynkevitch <basile@starynkevitch.net>
2012-10-16 16:24:26 +0200

    more on ex06, not working yet...

--- ex06/ex06c-jansson.c
+++ ex06/ex06c-jansson.c
@@ -1,8 +1,12 @@
 /* ex06c-jansson.c, a simple example to test jansson variadic typing */
 #include <stdlib.h>
 #include <jansson.h>
+#include <string.h>
 
-json_t *valid_packpair(int x, int y)
+json_t *valid_pack(int x, int y)
 {
-  return json_pack("{sisi}", "X", x, "Y", y);
+  return json_pack("{sisiss}", 
+		   "XX", x, 
+		   "YY", y,
+		   "ZZ", strdup("a"));
 }
