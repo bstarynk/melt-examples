c4b719be1a4e167c883695a4b5b6096dd415a275
Basile Starynkevitch <basile@starynkevitch.net>
2012-10-19 16:22:19 +0200

    improved ex06m type compare...

--- ex06/ex06c-jansson.c
+++ ex06/ex06c-jansson.c
@@ -1,4 +1,5 @@
-/* ex06c-jansson.c, a simple example to test jansson variadic typing */
+/* ex06c-jansson.c, a simple example to test jansson variadic typing,
+   in public domain */
 #include <stdlib.h>
 #include <jansson.h>
 #include <string.h>
@@ -8,5 +9,5 @@ json_t *valid_pack(int x, int y)
   return json_pack("{sisiss}", 
 		   "XX", x, 
 		   "YY", y,
-		   "ZZ", strdup("a"));
+		   "STR", strdup ("a"));
 }
